8a01a0367245ca1874bfe8f1c1aad1f3597cdd64
Yaser.ta <yasertarrab10@gmail.com>
2024-12-05 19:48:55 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+// 1) Task 
+let sehirler = ["Paris", "Istanbul", "Berlin", "Tokyo", "Dubai"];
+
+
+for (let i = 0; i < sehirler.length; i++) {
+    console.log(`Şehir ${i + 1}: ${sehirler[i]}`);
+}
+
+
+// 2) Task 2
+
+
+let sayilar = [12, 45, 78, 23, 56];
+
+
+console.log("Dizideki eleman sayisi:", sayilar.length);
+
+
+
+
+// 3) Task
+
+
+let dizi = [];
+
+
+dizi.push("Halep");
+dizi.push("Hama");
+dizi.push("Idlip");
+dizi.push("Sam");
+
+
+console.log("Sonuç:", dizi);
+
